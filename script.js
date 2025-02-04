e761440ff97320eb692e58e89ffd0cad518935b6
Roberto <robmor92x@gmail.com>
2025-02-04 21:23:26 +0100

    Create and make works dialog

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-const myLibrary = [];
+ $(document).ready(function() { 
+    const myLibrary = [];
+    $("#submit").on("click", getInfo);
+    $("#showDialog").on("click", () => {
+      $("#libraryDialog").showModal();
+    });
 
-function Book(name, author, numPages) {
-  // the constructor...
-  this.name = name;
-  this.author = author;
-  this.numPages = numPages;
-}
 
-function addBookToLibrary(book) {
-  // do stuff here
-    myLibrary.splice(-1,0,book) 
-}
-const book1= new Book("Daily Bugle", "JJJ","20");
-addBookToLibrary(book1);
+    function getInfo(){    
+      let newbook= new Book($("#name").val(), $("#author").val(), $("#numPages").val());
+      $("#libraryDialog").hide();
+    }
 
+    function Book(name, author, numPages) {
+        // the constructor...
+        this.name = name;
+        this.author = author;
+        this.numPages = numPages;
+    }
+      
+    function addBookToLibrary(book) {
+          myLibrary.splice(-1,0,book) 
+      }
+    
+    function showlibrary(){
+      libraryTable = document.querySelector("#library");
+        
+    }
+
+    const book1= new Book("Daily Bugle", "JJJ","20");
+    addBookToLibrary(book1);
+});
