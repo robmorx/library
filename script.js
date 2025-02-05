0f240a27fa3edfdcadeb2abd0e4f5bd3fb89feff
Roberto <robmor92x@gmail.com>
2025-02-05 20:26:50 +0100

    Show library table

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
  $(document).ready(function() { 
     const myLibrary = [];
-    $("#submit").on("click", getInfo);
+    $("#submit").on("click", (e)=>{
+      e.preventDefault();
+      getInfo();
+    });
     $("#showDialog").on("click", () => {
-      $("#libraryDialog").showModal();
+      const dialog = document.querySelector("#libraryDialog");
+      dialog.showModal();
     });
 
 
     function getInfo(){    
       let newbook= new Book($("#name").val(), $("#author").val(), $("#numPages").val());
+      
       $("#libraryDialog").hide();
     }
 
@@ -19,12 +24,24 @@
     }
       
     function addBookToLibrary(book) {
-          myLibrary.splice(-1,0,book) 
+          myLibrary.splice(-1,0,book);
+          showlibrary(); 
       }
     
     function showlibrary(){
       libraryTable = document.querySelector("#library");
-        
+      
+      for (let book in myLibrary){
+        tr = document.createElement("tr");
+        const keys = Object.keys[book];
+        for(let key in keys){
+          td = document.createElement("td");
+          td.innetText = book.key;
+          tr.appendChild(td);
+        }
+        libraryTable.appendChild(tr);
+      }
+
     }
 
     const book1= new Book("Daily Bugle", "JJJ","20");
